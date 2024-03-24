9fbedfbeb06d40fa81ddb389c85dff40f39c2635
MAlshaik <alshaik8@msu.edu>
2024-03-24 15:36:24 -0400

    salam

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Sup gang</h1> 
+       <h1>Asalamu Alaikum</h1> 
       </header>
     </div>
   );
